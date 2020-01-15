4d54a62721739dd5a95d267d595cced370c43251
Kevin Geary <kgeary@gmail.com>
2020-01-15 17:01:26 -0600

    Added additional fields to formatData to correct missing info. Disabled debug and temp files for release

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generator = require('./generateHTML.js');
 
 // Debug Flags
 const debug = false;
-const tempFiles = true;
+const tmpFiles = debug || false;
 
 // Questions to prompt the user with
 const questions = [
@@ -52,116 +52,116 @@ function writeToPdf(fileName, html) {
     });
 }
 
-// Get All User info needed from github
-// Returns an array of promises
-// Promise [0] - User Info
-// Promise [1] - Star Info
-function getUser(name) {
-    console.log("Name", name);
-    const path = `/users/${name}`;
-
-    // Get the User Data and the User Starred List
-    return Promise.all(
-        [
-            getApiData(path),
-            getApiData(path + "/starred"),
-        ]);
-}
-
-// Return a promise to get data from the github api
-// returns a response object
-function getApiData(path) {
-    /* Request option parameters */
-    const host = 'api.github.com';
-    return axios.get(`https://${host}${path}`);
-}
-
 // Format input data like we need it
 function formatData(data) {
+    // Create a maps link if location was specified 
     if (data.location) {
         data.map = "https://www.google.com/maps/place/" +
             data.location.replace(" ", "+");
     } else {
+        data.location = "No Location Provided";
         data.map = "#";
     }
 
-    if (!data.blog) { 
-        data.blog = "#"; 
+    // Set a default value for name if null
+    if (!data.name) {
+        data.name = "No Name Provided";
     }
+    // Set a default value for bio if null
+    if (!data.bio) {
+        data.bio = "No Bio Provided";    
+    }
+    // Set a default value for blog if null
+    if (!data.blog) {
+        data.blog = "#";
+    }
+}
+
+// Parse the api reponse data into an object with just the data we need
+function parseResponse(rspUser, rspStar, userColor) {
+    const data = {
+        name: rspUser.name,
+        login: rspUser.login,
+        img: rspUser.avatar_url,
+        bio: rspUser.bio,
+        blog: rspUser.blog,
+        location: rspUser.location,
+        url: rspUser.html_url,
+        publicRepos: rspUser.public_repos,
+        followers: rspUser.followers,
+        following: rspUser.following,
+        color: userColor,
+        stars: rspStar.length,
+    };
+
+    // Fix the data up before applying it to html
+    formatData(data);
+    return data;
 }
 
 // Initialize and run the script
 function init() {
-    // Prompt user with questions
-    // Generate a new HTML
-    // Generate a new PDF
-    let userColor;
-    let data = {};
+    let userColor; // Store the user color for use later
+    let data = {}; // Store data parameters needed to fill in the html
 
+    // Prompt user with questions
     inquirer
-        .prompt(questions)
+        .prompt(questions) 
         .then(function (response) {
-            // User Input Responses received
-            // store the color for later
-            // get API data returns array of promises
+            // Then after User Input Responses received
             userColor = response.color;
-            return getUser(response.name);
+            const name = response.name;
+
+            // get API data returns array of promises
+            return Promise.all(
+                [
+                    axios.get(`https://api.github.com/users/${name}`),
+                    axios.get(`https://api.github.com/users/${name}/starred`),
+                ]);
         })
         .then(function (responses) {
-            
-            // Set the response objects to the data of the responses
+            // Then after All API Calls returned successfully
+            // Set the response objects to the 'data' field of the responses
             const [rspUser, rspStar] = responses.map(x => x.data);
 
             // Convert API response object into params object
-            if (debug) {
-                console.log("User Api Response", rspUser);
-            }
+            if (debug) { console.log("User Api Response", rspUser); }
 
             // Save the user data to a variable
-            data = {
-                name: rspUser.name,
-                login: rspUser.login,
-                img: rspUser.avatar_url,
-                bio: rspUser.bio,
-                blog: rspUser.blog,
-                location: rspUser.location,
-                url: rspUser.html_url,
-                publicRepos: rspUser.public_repos,
-                followers: rspUser.followers,
-                following: rspUser.following,
-                color: userColor,
-                stars: rspStar.length,
-            };
+            data = parseResponse(rspUser, rspStar, userColor);
 
-            // Fix the data up before applying it to html
-            formatData(data);
- 
             if (debug) {
                 console.log("Star Api Response", rspStar);
-                console.log("table = params");
+                console.log("data object");
                 console.table(data);
             }
-            return generator.generateHTML(data);
-        }).then(function (html) {
-            // Write the HTML to a text file and to a PDF
-            const fname = (debug || tempFiles) ? 'temp' : 
-                `${data.name.replace(' ', '_')}`;
 
-            if (debug) {
-                writeToFile(`${fname}.html`, html);
-            }
+            // Generate the HTML based off the newly acquired data
+            const html = generator.generateHTML(data);
+        
+            // Get the filename to use for the output (temp when debugging else user_name)
+            const fname = tmpFiles ? 'temp' : `${data.name.replace(' ', '_')}`;
+            
+            // Write the html data to a Text File
+            if (debug) { writeToFile(`${fname}.html`, html); }
+
+            // Write the html data to a PDF
             writeToPdf(`${fname}.pdf`, html);
         }).then(function () {
+            // Then after the PDF was written.
+            // Everything completed successfully. Pack it up kids
             console.log("Success");
         })
         .catch(function (error) {
+            // Ruh roh - something went wrong
             console.log("CATCH-ERROR:", error.message);
             if (debug) {
                 console.log(error);
-            }   
+            }
         });
 }
 
+// Alert the user we are in debug mode. Extra output expected
 if (debug) {
     console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     console.log("Debug Mode Active");
