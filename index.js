aa1ac6ad69b114f4661f8c1f569caad3ad2f257e
Kevin Geary <kgeary@gmail.com>
2020-01-16 15:53:14 -0600

    Make tmp.html a hidden file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const generator = require('./generateHTML.js');
-const TMP_HTML = 'tmp.html';
+const TMP_HTML = '.tmp.html';
 
 // Debug Flags
 const debug = false;
