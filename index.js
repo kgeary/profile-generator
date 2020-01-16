fa722d733a932f91a3817065756b8c497a15dbf0
Kevin Geary <kgeary@gmail.com>
2020-01-16 17:32:33 -0600

    Cleanup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const generator = require('./generateHTML.js');
 const TMP_HTML = '.tmp.html';
 
 // Debug Flags
-const debug = false || (process.argv[2]==="debug");
+const debug = false || (process.argv[2] === "debug");
 const tmpFiles = debug || false;
 const DEFAULT_NAME = "No Name Provided";
 
@@ -46,8 +46,8 @@ async function writeToPdf(inFile, outFile) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const filePath = path.resolve(`${inFile}`);
-  await page.goto(`file:${filePath}`, {waitUntil: 'networkidle2'});
-  await page.pdf({path: `${outFile}.pdf`, format: 'A4'});
+  await page.goto(`file:${filePath}`, { waitUntil: 'networkidle2' });
+  await page.pdf({ path: `${outFile}.pdf`, format: 'A4' });
   // If debug - create a screenshot image
   if (debug) {
     await page.setViewport({
@@ -73,11 +73,11 @@ function formatData(data) {
 
   // Set a default value for name if null
   if (!data.name) {
-    data.name = DEFAULT_NAME;
+    data.name = data.login;
   }
   // Set a default value for bio if null
   if (!data.bio) {
-    data.bio = "No Bio Provided";
+    data.bio = "";
   }
   // Set a default value for blog if null
   if (!data.blog) {
@@ -109,14 +109,13 @@ function parseResponse(rspUser, rspStar, userColor) {
 
 // Initialize and run the script
 async function init() {
-  let userColor; // Store the user color for use later
   let data = {}; // Store data parameters needed to fill in the html
   let fname;
   try {
     // Prompt user with questions
-    const response = await inquirer.prompt(questions);
-    const name = response.name;
-    userColor = response.color;
+    const userResponse = await inquirer.prompt(questions);
+    const name = userResponse.name;
+    const userColor = userResponse.color;
 
     // get API data returns array of promises
     const responses = await Promise.all(
@@ -149,30 +148,38 @@ async function init() {
     fname = tmpFiles ? 'temp' : `${data.name.replace(/\s/g, '_')}`;
 
     // Write the html data to a Text File
-    await writeToFile(TMP_HTML, html); 
+    await writeToFile(TMP_HTML, html);
 
     // Write the html data to a PDF
     await writeToPdf(TMP_HTML, `${fname}`);
-    
+
     console.log("Success");
   } catch (error) {
-    console.log("CATCH-ERROR:", error.message);
+
     if (debug) {
       console.log(error);
     }
+    // Handle Error
+    if (error.response.status === 404) {
+      console.log("ERROR:", "User Not Found!");
+    }
+    else { // Display the provided error message
+      console.log("ERROR:", error.message);
+    }
+
   } finally {
     // Delete the temporary HTML file if it exists
-    if (fs.existsSync(TMP_HTML) && !debug) {
+    if (fs.existsSync(TMP_HTML)) {
       const unlinkAsync = util.promisify(fs.unlink);
       await unlinkAsync(TMP_HTML);
     }
   }
 }
 
-  // Alert the user we are in debug mode. Extra output expected
-  if (debug) {
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    console.log("Debug Mode Active");
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-  }
-  init();
+// Alert the user we are in debug mode. Extra output expected
+if (debug) {
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  console.log("Debug Mode Active");
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
+init();
